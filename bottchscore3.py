6e8800d0967bc5bbe85b77568e7bd7a4b9445d39
Stefano Forli <forli@scripps.edu>
2020-07-22 21:15:38 -0700

    changed CLI interface; improved Vi calculation, automorphism/symmetry

--- bottchscore3.py
+++ bottchscore3.py
@@ -30,10 +30,11 @@ from math import log
 
 # TODO 
 # - missing atropisomers (https://en.wikipedia.org/wiki/Atropisomer)
-# - according to the original implementation, t-butyl and isopropyl have same complexity?
+# - according to the original implementation, t-butyl and isopropyl have sam complexiy?
+
 
 class BottchScore:
-    def __init__(self, verbose=False):
+    def __init__(self, verbose=False, debug=False):
         """ 
             TERMS:
             
@@ -44,23 +45,87 @@ class BottchScore:
             b_i :   sum of all bond orders
         
         """
+        self.converter = ob.OBConversion()
+        self.converter.SetOutFormat('smi')
         self.verbose=verbose
+        self.debug=debug
+        self._mesomery_patterns = {
+                # SMARTS_pattern : [equivalent atoms idx list, contribution ]
+                '[$([#8;X1])]=*-[$([#8;X1])]' : [ [[0,2]], 1.5], # carboxylate, nitrate
 
-    def score(self, mol):
+                #'[$([#8;X1])]~*~[$([#8;X1])]' : [ [0,2], 1.5], # carboxylate, nitrate
+                #'[$([#8;X1])]~*~[$([#8;X2,H1])]' : [ [0,2], 1.5], # carboxylate, nitrate
+                #'[$([#7;X2](=*)[*=*])]'
+                '[$([#7;X2](=*)(-*=*(-*)))]' :  [ [[]],  1.5 ], # guanidine, azire
+                    
+                #'[CH2X4]' # histidine
+                '[#6X3]1:' # imidazole
+                '[$([#7X3H+,#7X2H0+0]:[#6X3H]:[#7X3H]),$([#7X3H])]:'
+                '[#6X3H]:'
+                '[$([#7X3H+,#7X2H0+0]:[#6X3H]:[#7X3H]),$([#7X3H])]:'
+                '[#6X3H]1' :  [[[1,3]], 2.5],
+                }
+
+    def score(self, mol, disable_mesomeric=False):
         """ """
-        if mol.NumAtoms()==0:
-            print("*** WARNING: NO ATOMS FOR MOLECULE [%s] ***" % mol.GetTitle())
-            return
-        self.mol = mol
-        # create the storage for the non-hydrogen atoms
-        self._build_automorphism()
-        self._indices = OrderedDict()
-        self._equivalents = {}
+        if self.debug:
+            print("==========================================")
+        if mol.NumAtoms()<2:
+            #print("*** WARNING: NO ATOMS FOR MOLECULE [%s] ***" % mol.GetTitle())
+            return 0
+            #return
+        self._initialize_mol(mol, disable_mesomeric)
         self._calculate_terms()
         self._calculate_score()
         if self.verbose:
             self.print_table()
         return self._intrinsic_complexity
+
+    def _initialize_mol(self, mol, disable_mesomeric):
+        """ perform initial operations on the molecule caching chemical
+        information for aromatic and some tautomeric perceptions"""
+        self.mol = mol
+        #if self.verbose:
+        full = self.converter.WriteString(self.mol)
+        self._smiles = full.split()[0]
+        if self.debug: print("DEBUG> SMILES: %s" % self._smiles)
+        # create the storage for the non-hydrogen atoms
+        self._build_automorphism()
+        self._indices = OrderedDict()
+        self._mesomery_equivalence = {}
+        if not disable_mesomeric:
+            self._calc_mesomery()
+
+    def _calc_mesomery(self):
+        """calculate atoms for which b_i value needs to be corrected"""
+        matcher = ob.OBSmartsPattern()
+        for patt, idx_info in self._mesomery_patterns.items():
+            #print("IDXINFO", idx_info)
+            idx_pairs, contribution = idx_info
+            matcher.Init(patt)
+            found = matcher.Match(self.mol)
+            if not found:
+                #print ("NADA")
+                continue
+            if self.debug: print("DEBUG> Matched pattern |%s|"% patt)
+            found = [list(x) for x in matcher.GetUMapList()]
+            for f in found:
+                for pair in idx_pairs:
+                    for idx in pair:
+                        if self.debug: print("DEBUG> Assigning mesomery:",f, "->", f[idx])
+                        self._mesomery_equivalence[f[idx]] = contribution
+                    p0 = f[pair[0]]
+                    p1 = f[pair[1]]
+                    if self.debug: print("DEBUG> Updating automorphs:", p0,p1 )
+                    if not p0 in self.automorphs:
+                        self.automorphs[p0] = set()
+                    self.automorphs[p0].add(p1)
+                    if not p1 in self.automorphs:
+                        self.automorphs[p1] = set()
+                    self.automorphs[p1].add(p0)
+        if self.debug:
+            print("DEBUG> MESO-EQUIV", self._mesomery_equivalence)
+            print("DEBUG> AUTO-EQUIV", self.automorphs)
 
     def _calculate_terms(self):
         """ calculate the different terms contribution"""
@@ -94,8 +159,8 @@ class BottchScore:
                     print("%d\t" % self._indices[i][prop], end=' ')
             print("")
         print('---------------------------------------')
-        print("Intrinsic complexity : %2.1f" % self._intrinsic_complexity)
-        print("Complexity/atom      : %2.1f" % (self._intrinsic_complexity/len(self._indices)))
+        print("Intrinsic complexity : %2.2f" % self._intrinsic_complexity)
+        print("Complexity/atom      : %2.2f" % (self._intrinsic_complexity/len(self._indices)))
         print("===========================================================")
         
 
@@ -105,6 +170,8 @@ class BottchScore:
         """
         total_complexity = 0
         # eq.3 of ref.1, left side
+        if self.verbose:
+            print(">> %s" % self._smiles)
         for idx in list(self._indices.keys()):
             complexity = self._calculate_complexity(idx)
             self._indices[idx]['complexity'] = complexity
@@ -119,40 +186,64 @@ class BottchScore:
         """ perform calculation of single complexity on a given index"""
         data = self._indices[idx]
         if self.verbose:
-            print(("complexty [%3d]:  %d * %d * %d log2( %d * %d) = %2.1f"  % (idx, 
-                data['di'], data['ei'],data['si'], data['Vi'], data['bi'], 
-                data['di']*data['ei']*data['si']*log(data['Vi']*data['bi'], 2)
-                )))
-
-        return data['di']*data['ei']*data['si']*log(data['Vi']*data['bi'], 2)
-
+            try:
+                print(("complexity [%3d]:  %d * %d * %d log2( %d * %d) = %2.1f"  % (idx, 
+                    data['di'], data['ei'],data['si'], data['Vi'], data['bi'], 
+                    data['di']*data['ei']*data['si']*log(data['Vi']*data['bi'], 2)
+                    )))
+            except:
+                print("[ *** Error calculating complexity: atom_idx[%d] *** ]" % idx)
+        try:
+            return data['di']*data['ei']*data['si']*log(data['Vi']*data['bi'], 2)
+        except:
+            print("[ *** Error calculating complexity: atom_idx[%d] *** ]" % idx)
+            return 0
 
     def _calc_Vi(self, idx, atom):
         """ """
-        num = atom.GetAtomicNum()
-        val = 8-ob.GetMaxBonds(num)
-        self._indices[idx]['Vi'] = val
-
+        valence = atom.GetTotalValence()
+        charge = atom.GetFormalCharge()
+        if self.debug: print("DEBUG> Vi[%3d]: v = %d | charge = %d"% (idx, valence, charge))
+        vi = 8 - valence + charge
+        self._indices[idx]['Vi'] = vi
 
     def _calc_bi_ei_si(self, idx, atom):
         """ """
         bi = 0
-        ei = [atom.GetAtomicNum()]
-        #this_element = atom.GetAtomicNum()
+        ei = [self._get_atomic_num(atom)]
+        atom_idx = atom.GetIdx()
         si = 1
         if atom.IsChiral():
             si+=1
         for neigh in ob.OBAtomAtomIter(atom):
-            if self._is_hydrogen(neigh.GetIdx()):
+            neigh_idx = neigh.GetIdx()
+            if self._is_hydrogen(neigh_idx):
                 continue
-            bond = self.mol.GetBond(atom, neigh)
-            # get bond order
-            bi += bond.GetBondOrder()
+            if atom_idx in self._mesomery_equivalence:
+                # use mesomeric-corrected bond order
+                #print("FOUNX", atom_idx, atom_idx in self._mesomery_equivalence, self._mesomery_equivalence)
+                contribution = self._mesomery_equivalence[atom_idx]
+            else:
+                # get bond order
+                bond = self.mol.GetBond(atom, neigh)
+                contribution = bond.GetBondOrder()
+            #print("BONDORDER B_i", contribution)
+            bi += contribution
             # get neighbor element
-            ei.append(neigh.GetAtomicNum())
+            ei.append(self._get_atomic_num(neigh))
         self._indices[idx]['bi'] = bi
         self._indices[idx]['ei'] = len(set(ei))
         self._indices[idx]['si'] = si
+
+    def _get_atomic_num(self, atom):
+        """ function to return the atomic number taking into account 
+            isotope
+        """
+        isotope = atom.GetIsotope()
+        if not isotope==0:
+            return isotope
+        return atom.GetAtomicNum()
+        
 
     def _calc_di(self, idx, atom):
         """ """
@@ -165,8 +256,6 @@ class BottchScore:
                     self._equivalents[idx]=self.automorphs[idx]
             else:
                 self._equivalents[idx]=set()
-            
-            #self._equivalents[idx] = []
         if idx in self.automorphs:
             for u in self.automorphs[idx]:
                 self._equivalents[idx].append(u)
@@ -182,15 +271,22 @@ class BottchScore:
             groups.append(neigh_idx)
         self._indices[idx]['di'] = len(groups)
 
-
     def _build_automorphism(self):
         """ automorphisms are 0-based
             prune the automorphism map to remove
             identities and compact multiple
             mappings
         """
+        #itab = ob.OBIsitopeTable()
+        self._equivalents = {}
         automorphs = ob.vvpairUIntUInt()
-        ob.FindAutomorphisms(self.mol, automorphs)
+        mol_copy = ob.OBMol(self.mol)
+        for i in ob.OBMolAtomIter(mol_copy):
+            isotope = i.GetIsotope()
+            if not isotope==0:
+                n = i.GetAtomicNum()
+                i.SetAtomicNum(n+isotope)
+        ob.FindAutomorphisms(mol_copy, automorphs)
         self.automorphs = {}
         for am in automorphs:
             for i,j in am:
@@ -207,10 +303,12 @@ class BottchScore:
                 self.automorphs[k].append(l)
         for k,v in list(self.automorphs.items()):
             self.automorphs[k] = set(v)
-        if self.verbose:
+        if self.debug:
             from pprint import pprint
-            print("AUTOMORPHS")
+            print("AUTOMORPHS", end='')
             pprint(self.automorphs)
+        #elif self.debug:
+        #    print("DEBUG> Automorphs:", self.automorphs)
 
     def _is_hydrogen(self, idx):
         return self.mol.GetAtom(idx).GetAtomicNum()==1
@@ -220,24 +318,31 @@ if __name__=='__main__':
     import sys
     import os
     import argparse
-    usage = "Usage: %s -i molfile.ext [-p] [-v]" % sys.argv[0]
+    debug = False
+    usage = "%s -i molfile.ext [-p] [-v] " % sys.argv[0]
     epilog = ""
-    parser = argparse.ArgumentParser(description='Tool to calculate Böttcher score on small molecules.', usage=usage, 
+    parser = argparse.ArgumentParser(description='Tool to calculate Böttcher score on small molecules.', usage=None, 
                 epilog=epilog, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-i', action="store", help='input structure; support all input formats supported by OB, including multi-structure formats', metavar='filename.ext', required=True)
+    parser.add_argument('-m', action="store_true", help='disable mesomeric effect estimate', default=False)
     parser.add_argument('-p', action="store_true", help='generate PNG image of the structure', default=False)
     parser.add_argument('-v', action="store_true", help='verbose mode; print the full table of the terms used to estimate the score, as described in the paper', default=False)
+    #parser.add_argument('-s', action="store", help='SMILES input mode')
     if len(sys.argv)<2:
         parser.print_help()
         sys.exit(1)
+    if "-d" in sys.argv:
+        debug=True
+        sys.argv.remove('-d')
     ARGS = parser.parse_args()
-
     # input file
     infile = ARGS.i
     # check if verbose
     verbose = ARGS.v
     # save image
     save_png = ARGS.p
+    # mesomeric effect
+    disable_mesomeric = ARGS.m
     # Parse format
     name, ext = os.path.splitext(infile)
     ext = ext[1:].lower()
@@ -246,14 +351,34 @@ if __name__=='__main__':
     mol_parser = ob.OBConversion()
     mol_parser.SetInAndOutFormats(ext,'smi')
     # initialize class
-    bottch = BottchScore(verbose)
+    bottch = BottchScore(verbose, debug)
     # load the first molecule found in the file
     mol = ob.OBMol()
     more = mol_parser.ReadFile(mol, infile)
+    while more:
+        ob.PerceiveStereo(mol)
+        # score the molecule
+        score=bottch.score(mol, disable_mesomeric)
+        if not verbose:
+            print("%d: %4.2f\t %s"% (counter,score,mol.GetTitle()))
+        if save_png:
+            name = mol.GetTitle()
+            if name.strip()=="":
+                name = "mol_%d" % counter
+            mol_parser.SetOutFormat('png')
+            mol_parser.WriteFile(mol, '%s_image.png' % name)
+        more = mol_parser.Read(mol)
+        counter+=1
+
+"""
+    sys.exit(1)
+
+    more = mol_parser.ReadFile(mol, infile)
+    ob.PerceiveStereo(mol)
     # score the molecule
     score=bottch.score(mol)
     if not verbose:
-        print("%d: %2.1f"% (counter,score))
+        print("%d: %2.2f"% (counter,score))
     if save_png:
         name = mol.GetTitle()
         if name.strip()=="":
@@ -264,12 +389,13 @@ if __name__=='__main__':
     while more:
         mol = ob.OBMol()
         more = mol_parser.Read(mol)
+        ob.PerceiveStereo(mol)
         if not more:
             break
         counter+=1
         score = bottch.score(mol)
         if not verbose:
-            print("%d: %2.1f"% (counter,score))
+            print("%d: %2.2f"% (counter,score))
         if save_png:
             name = mol.GetTitle()
             if name.strip()=="":
@@ -278,4 +404,5 @@ if __name__=='__main__':
             mol_parser.WriteFile(mol, '%s_image.png' % name)
 
 
+"""
 
